3826d2676e51c8b5f3beb848e35a5cdebfbe3f96
emmersonn <102638044+emmersonn@users.noreply.github.com>
2023-02-26 17:39:34 -0500

    multiple

---
+++ client/lab_3/images/Homework1.py
@@ -0,0 +1,50 @@
+
+
+from argparse import ArgumentParser
+import sys
+import math
+
+def get_min_payement():
+
+
+
+def interest_charged():
+    
+def remaining_payments():
+    
+def main():
+
+def parse_args(args_list):
+    """Takes a list of strings from the command prompt and passes them through as
+    arguments
+    Args:
+        args_list (list) : the list of strings from the command prompt
+    Returns:
+        args (ArgumentParser)
+    """
+    parser = ArgumentParser()
+    parser.add_argument('balance_amount', type = float, help = 'The total amount of balance left on the credit account')
+    parser.add_argument('apr', type = int, help = 'The annual APR, should be an int between 1 and 100')
+    parser.add_argument('credit_line', type = int, help = 'The maximum amount of balance allowed on the credit line.')
+    parser.add_argument('--payment', type = int, default = None, help = 'The amount the user wants to pay per payment, should be a positive number')
+    parser.add_argument('--fees', type = float, default = 0, help = 'The fees that are applied monthly.')
+    # parse and validate arguments
+    args = parser.parse_args(args_list)
+    if args.balance_amount < 0:
+        raise ValueError("balance amount must be positive")
+    if not 0 <= args.apr <= 100:
+        raise ValueError("APR must be between 0 and 100")
+    if args.credit_line < 1:
+        raise ValueError("credit line must be positive")
+    if args.payment is not None and args.payment < 0:
+        raise ValueError("number of payments per year must be positive")
+    if args.fees < 0:
+        raise ValueError("fees must be positive")
+    return args
+if __name__ == "__main__":
+    try:
+        arguments = parse_args(sys.argv[1:])
+    except ValueError as e:
+        sys.exit(str(e))
+    print(main(arguments.balance_amount, arguments.apr, credit_line = 
+arguments.credit_line, targetamount = arguments.payment, fees = arguments.fees))
