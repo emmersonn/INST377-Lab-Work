2de442c6c420ba6de3391be8b6a12e93a566f857
emmersonn <102638044+emmersonn@users.noreply.github.com>
2023-03-15 14:04:56 -0400

    index.html

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,41 +1,35 @@
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
+
+
+
+
 function getRandomIntInclusive(min,max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1) + min);
-
 }
-
 
 function injectHTML(list){
   console.log('fired inject HTML')
   const target = document.querySelector('#restaurant_list')
   target.innerHTML = '';
-  list.forEach ((item,index ) => {
+  list.forEach ((item) => {
     const str = `<li>${item.name}<li>`;
     target.innerHTML += str
   })
-
 }
+
 /* A quick filter that will return something based on a matching input */
 function filterList(list, query) {
   return list.filter((item) => {
     const lowerCaseName = item.name.toLowerCase();
     const lowerCaseQuery = query.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
+   })
+}
 
+function cutRestaurantList (){
+  console.log('fired cut list');
 
-  })
-  /*
-    Using the .filter array method, 
-    return a list that is filtered by comparing the item name in lower case
-    to the query in lower case
-
-    Ask the TAs if you need help with this
-  */
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
@@ -43,33 +37,22 @@ async function mainEvent() { // the async keyword means we can make API requests
   const filterButton = document.querySelector('#filter');
   const loadDataButton = document.querySelector('#data_load');
   const generateListButton = document.querySelector('#generate');
-
-  // Add a querySelector that targets your filter button here
-
   let currentList = []; // this is "scoped" to the main event function
-  
+
   /* We need to listen to an "event" to have something happen in our page - here we're listening for a "submit" */
-  mainForm.addEventListener('submit', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
-    submitEvent.preventDefault(); 
-    console.log('form submission'); 
+  loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something
+      submitEvent.preventDefault(); 
+      console.log('form submission'); 
 
-    /*
-      ## GET requests and Javascript
-        We would like to send our GET request so we can control what we do with the results
-        Let's get those form results before sending off our GET request using the Fetch API
     
-      ## Retrieving information from an API
-        The Fetch API is relatively new,
-        and is much more convenient than previous data handling methods.
-        Here we make a basic GET request to the server using the Fetch method to the county
-    */
-
-    // Basic GET request - this replaces the form Action
+      // Basic GET request - this replaces the form Action
     const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
-    console.log(results);
+    
+
 
 
     // This changes the response from the GET into data we can use - an "object"
+    console.log(results); //find new place
     currentList = await results.json();
     console.table(currentList); 
     injectHTML(currentList);
@@ -85,6 +68,10 @@ async function mainEvent() { // the async keyword means we can make API requests
     const newList  = filterList(currentList, formProps.resto); 
     injectHTML(currentList);
     console.log(newList);
+    })
+
+    generateListButton.assEventListener('click',(event) =>{
+
     })
 
   
